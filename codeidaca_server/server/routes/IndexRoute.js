62b7151f14ef30dbb2f382fb6ceac1aa92025980
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2022-08-31 01:17:58 +0700

    Update IndexRoute.js

--- codeidaca_server/server/routes/IndexRoute.js
+++ codeidaca_server/server/routes/IndexRoute.js
@@ -5,6 +5,8 @@ import AddressTypeRoute from './AddressTypeRoute'
 import CountryRoute from './CountryRoute'
 import ProvinceRoute from './ProvinceRoute'
 import CityRoute from './CityRoute'
+import ListRoute from './ListRoute'
+import TestRoute from './TestRoute'
 
 export default{
     UserRoute,
@@ -13,5 +15,7 @@ export default{
     AddressTypeRoute,
     CountryRoute,
     ProvinceRoute,
-    CityRoute
+    CityRoute,
+    ListRoute,
+    TestRoute
 }
