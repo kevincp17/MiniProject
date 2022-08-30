6e37ddf4acaf2966391db0bbc71c6a37beaae3cc
HarryRoll <101006652+HarryRoll@users.noreply.github.com>
2022-08-30 20:27:05 +0700

    melakukan import cateCtrl dan parentCateCtrl pada indexController.js

--- codeidaca_server/server/controller/IndexController.js
+++ codeidaca_server/server/controller/IndexController.js
@@ -5,6 +5,8 @@ import AddressTypeController from "./AddressTypeController"
 import CountryController from "./CountryController"
 import ProvinceController from "./ProvinceController"
 import CityController from "./CityController"
+import cateCtrl from "./categoryCtrl"
+import parentCateCtrl from "./parentCateCtrl"
 
 export default{
     UserController,
@@ -13,5 +15,7 @@ export default{
     AddressTypeController,
     CountryController,
     ProvinceController,
-    CityController
+    CityController,
+    cateCtrl,
+    parentCateCtrl,
 }
