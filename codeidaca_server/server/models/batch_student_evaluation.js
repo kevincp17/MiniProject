bdf6cd69a4cdc0b9a9866edc59c8b01d27995878
fryn777 <febriansudiprasetya@gmail.com>
2022-08-31 09:41:26 +0700

    Update config dan models

--- codeidaca_server/server/models/batch_student_evaluation.js
+++ codeidaca_server/server/models/batch_student_evaluation.js
@@ -1,9 +1,6 @@
-import _sequelize from 'sequelize';
-const { Model, Sequelize } = _sequelize;
-
-export default class batch_student_evaluation extends Model {
-  static init(sequelize, DataTypes) {
-  return super.init({
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('batch_student_evaluation', {
     base_id: {
       autoIncrement: true,
       type: DataTypes.INTEGER,
@@ -57,5 +54,4 @@ export default class batch_student_evaluation extends Model {
       },
     ]
   });
-  }
-}
+};
