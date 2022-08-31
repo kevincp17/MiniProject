7a44911f189e18f64c8d9dfcd8f720a519937a2f
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2022-08-31 14:33:38 +0700

    Update EditBatch.js
    remove unnecessary lines of code

--- codeidaca_client/src/views/app/batch/EditBatch.js
+++ codeidaca_client/src/views/app/batch/EditBatch.js
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useFormik } from 'formik'
-import { GetOneBatchRequestList, EditBatchListRequest } from '../../../redux-saga/actions/BatchListAction';
+import * as Yup from 'yup'
+import { GetOneBatchRequestList, EditBatchListRequest, GetBatchRequestList } from '../../../redux-saga/actions/BatchListAction';
 
 export default function EditBatch(props) {
     const dispatch = useDispatch()
-    const [batch, setBatch] = useState([])
-    const [refresh, setRefresh] = useState(false)
     const { listOne } = useSelector(state => state.batchListState)
     const { list } = useSelector(state => state.batchListState)
-
 
     useEffect(() => {
         dispatch(GetOneBatchRequestList(props.id))
     }, [])
+
     useEffect(() => {
         console.log('isi listone')
         console.log(listOne)
@@ -42,43 +41,27 @@ export default function EditBatch(props) {
             props.onRefresh();
         }
     })
-    
+
     return (
         <div>
             <div className='mx-auto overflow-hidden shadow-2xl h-full py-3 bg-slate-100' style={{ width: "290px", height: "200px", marginTop: "200px" }}>
                 <div className='mx-2 my-2'>
 
-                <div className='flex align-items-center'>
-                    <div className='my-auto mr-2'>
-                            
-                        </div>
-                        <label
-                            className="mt-1 ml-1 focus:ring-indigo-500 focus:border-indigo-500 block w-4/5 shadow-sm sm:text-sm border-gray-300 rounded-md "
-                            type="text"
-                            name="bast_review"
-                            id="bast_review"
-                            value={formik.values.bast_review}
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                            autoComplete="bast_review"
-                        />
-                    </div>
-
                     <div className='flex align-items-center'>
                         <div className='my-auto mr-2'>
                             Status
                         </div>
-                        <select
-                            className="rounded w-3/4"
-                            name="bast_status"
-                            id="bast_status"
-                            value={formik.values.bast_status}
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                            autoComplete="bast_status"
+                   <select
+                        className="rounded w-3/4"
+                        name="bast_status"
+                        id="bast_status"
+                        value={formik.values.bast_status}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        autoComplete="bast_status"
                         >
-                            <option value="Stop">Stop</option>
-                            <option value="Running">Running</option>
+                        <option value="Stop">Stop</option>
+                        <option value="Running">Running</option>
                         </select>
                     </div>
                     <div className='flex align-items-center'>
@@ -105,4 +88,3 @@ export default function EditBatch(props) {
         </div>
     )
 }
-
