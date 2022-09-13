729351f0d080b0a669af630394c597219e439f1d
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2022-09-13 22:07:25 +0700

    Delete TestRoute.js
    update

--- codeidaca_server/server/routes/TestRoute.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import IndexController from "../controller/IndexController";
-
-const router = Router()
-router.get('/', IndexController.Test.test)
-// router.get('/',IndexController.Test.findAll)
-
-
-export default router
